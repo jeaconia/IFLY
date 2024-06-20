09ca6e82e59faaea837359513ef6e010d63b4b4d
jeaconia <tjeaconia8@gmail.com>
2024-06-21 01:56:03 +0800

    models customers

---
+++ src/Customer.java
@@ -0,0 +1,38 @@
+public class Customer {
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
