c6a4fa485b4f5b6ec6eedc6c3a65e47d7ac979a3
jeaconia <tjeaconia8@gmail.com>
2024-06-21 02:09:40 +0800

    model subscription

--- src/Subscription.java
+++ src/Subscription.java
@@ -1,4 +1,67 @@
 import java.util.Date;
 public class Subscription {
+    private int id;
+    private int customer;
+    private int billingPeriod;
+    private String billingPeriodUnit;
+    private double totalDue;
+    private Date activatedAt;
+    private Date currentTermStart;
+    private Date currentTermEnd;
+    private String status;
 
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getCustomer() {
+        return customer;
+    }
+    public void setCustomer(int customer) {
+        this.customer = customer;
+    }
+    public int getBillingPeriod() {
+        return billingPeriod;
+    }
+    public void setBillingPeriod(int billingPeriod) {
+        this.billingPeriod = billingPeriod;
+    }
+    public String getBillingPeriodUnit() {
+        return billingPeriodUnit;
+    }
+    public void setBillingPeriodUnit(String billingPeriodUnit) {
+        this.billingPeriodUnit = billingPeriodUnit;
+    }
+    public double getTotalDue() {
+        return totalDue;
+    }
+    public void setTotalDue(double totalDue) {
+        this.totalDue = totalDue;
+    }
+    public Date getActivatedAt() {
+        return activatedAt;
+    }
+    public void setActivatedAt(Date activatedAt) {
+        this.activatedAt = activatedAt;
+    }
+    public Date getCurrentTermStart() {
+        return currentTermStart;
+    }
+    public void setCurrentTermStart(Date currentTermStart) {
+        this.currentTermStart = currentTermStart;
+    }
+    public Date getCurrentTermEnd() {
+        return currentTermEnd;
+    }
+    public void setCurrentTermEnd(Date currentTermEnd) {
+        this.currentTermEnd = currentTermEnd;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
