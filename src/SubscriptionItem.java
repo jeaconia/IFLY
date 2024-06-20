b54c8e4b933f58d0cdbcb0317ee42f7f003a0f13
jeaconia <tjeaconia8@gmail.com>
2024-06-21 02:11:11 +0800

    model subscription item

---
+++ src/SubscriptionItem.java
@@ -0,0 +1,45 @@
+public class SubscriptionItem {
+    private int id;
+    private int subscription;
+    private int item;
+    private int quantity;
+    private double price;
+    private double amount;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getSubscription() {
+        return subscription;
+    }
+    public void setSubscription(int subscription) {
+        this.subscription = subscription;
+    }
+    public int getItem() {
+        return item;
+    }
+    public void setItem(int item) {
+        this.item = item;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
